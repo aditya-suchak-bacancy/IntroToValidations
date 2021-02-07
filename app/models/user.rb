48586095e5965b830def08ebf2e6caa500caeda0
Aditya <adityasuchak24@gmail.com>
2021-02-07 17:35:15 +0530

    Configure Index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+    validates :name, presence: true
+    validates :last_name, presence: true
+    validates :dob, presence: true
+    validates :gender, presence: true
+    validates :address, presence: true
+    validates :pin, presence: true
+    validates :contact_no, presence: true
+
 end
