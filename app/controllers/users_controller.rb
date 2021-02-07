ac62d9a063a5ad688d7251aaec815faff176fbc2
Aditya <adityasuchak24@gmail.com>
2021-02-07 19:24:40 +0530

    Add New User feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,24 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name,:last_name,:dob,:gender,:address,:pin,:contact_no)
+    end
 end
